437cdd729ca83611f54feba97831524e8e2b0d93
iblug <1115jkb@gmail.com>
2023-06-01 17:26:15 +0900

    Add payments portone base

--- carts/static/payments/js/order_page.js
+++ carts/static/payments/js/order_page.js
@@ -85,3 +85,99 @@ function eximbayPay() {
     //rsp.imp_uid 값으로 결제 단건조회 API를 호출하여 결제결과를 판단합니다.
   });
 }
+function danalPay() {
+  IMP.request_pay({
+    pg: "danal",
+    // pay_method: "card",
+    // merchant_uid: "ORD20180131-0000011",   // 주문번호
+    name: orderItem,
+    amount: totalAmount,                         // 숫자 타입
+    // buyer_email: "gildong@gmail.com",
+    // buyer_name: "홍길동",
+    // buyer_tel: "010-4242-4242",
+    // buyer_addr: "서울특별시 강남구 신사동",
+    // buyer_postcode: "01181"
+  }, function (rsp) { // callback
+    //rsp.imp_uid 값으로 결제 단건조회 API를 호출하여 결제결과를 판단합니다.
+  });
+}
+function danalPay2() {
+  IMP.request_pay({
+    pg: "danal_tpay",
+    // pay_method: "card",
+    // merchant_uid: "ORD20180131-0000011",   // 주문번호
+    name: orderItem,
+    amount: totalAmount,                         // 숫자 타입
+    // buyer_email: "gildong@gmail.com",
+    // buyer_name: "홍길동",
+    // buyer_tel: "010-4242-4242",
+    // buyer_addr: "서울특별시 강남구 신사동",
+    // buyer_postcode: "01181"
+  }, function (rsp) { // callback
+    //rsp.imp_uid 값으로 결제 단건조회 API를 호출하여 결제결과를 판단합니다.
+  });
+}
+function tossPay() {
+  IMP.request_pay({
+    pg: "tosspay",
+    // pay_method: "card",
+    // merchant_uid: "ORD20180131-0000011",   // 주문번호
+    name: orderItem,
+    amount: totalAmount,                         // 숫자 타입
+    // buyer_email: "gildong@gmail.com",
+    // buyer_name: "홍길동",
+    // buyer_tel: "010-4242-4242",
+    // buyer_addr: "서울특별시 강남구 신사동",
+    // buyer_postcode: "01181"
+  }, function (rsp) { // callback
+    //rsp.imp_uid 값으로 결제 단건조회 API를 호출하여 결제결과를 판단합니다.
+  });
+}
+function paycoPay() {
+  IMP.request_pay({
+    pg: "payco",
+    // pay_method: "card",
+    // merchant_uid: "ORD20180131-0000011",   // 주문번호
+    name: orderItem,
+    amount: totalAmount,                         // 숫자 타입
+    // buyer_email: "gildong@gmail.com",
+    // buyer_name: "홍길동",
+    // buyer_tel: "010-4242-4242",
+    // buyer_addr: "서울특별시 강남구 신사동",
+    // buyer_postcode: "01181"
+  }, function (rsp) { // callback
+    //rsp.imp_uid 값으로 결제 단건조회 API를 호출하여 결제결과를 판단합니다.
+  });
+}
+function paypalPay() {
+  IMP.request_pay({
+    pg: "paypal",
+    // pay_method: "card",
+    // merchant_uid: "ORD20180131-0000011",   // 주문번호
+    name: orderItem,
+    amount: totalAmount,                         // 숫자 타입
+    // buyer_email: "gildong@gmail.com",
+    // buyer_name: "홍길동",
+    // buyer_tel: "010-4242-4242",
+    // buyer_addr: "서울특별시 강남구 신사동",
+    // buyer_postcode: "01181"
+  }, function (rsp) { // callback
+    //rsp.imp_uid 값으로 결제 단건조회 API를 호출하여 결제결과를 판단합니다.
+  });
+}
+function smilePay() {
+  IMP.request_pay({
+    pg: "smilepay",
+    // pay_method: "card",
+    // merchant_uid: "ORD20180131-0000011",   // 주문번호
+    name: orderItem,
+    amount: totalAmount,                         // 숫자 타입
+    // buyer_email: "gildong@gmail.com",
+    // buyer_name: "홍길동",
+    // buyer_tel: "010-4242-4242",
+    // buyer_addr: "서울특별시 강남구 신사동",
+    // buyer_postcode: "01181"
+  }, function (rsp) { // callback
+    //rsp.imp_uid 값으로 결제 단건조회 API를 호출하여 결제결과를 판단합니다.
+  });
+}
