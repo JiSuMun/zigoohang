d42f62d893be8726583a25a1e3a67f9b6e3f699e
JiSuMun <mjs3827@naver.com>
2023-06-07 00:35:52 +0900

    update chat room css

--- chat/consumers.py
+++ chat/consumers.py
@@ -3,6 +3,8 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
 from .models import ChatRoom, Message, Notification
 from django.contrib.auth import get_user_model
+from asgiref.sync import async_to_sync
+from channels.layers import get_channel_layer
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -35,7 +37,6 @@ class ChatConsumer(AsyncWebsocketConsumer):
             }
         )
 
-
     async def chat_message(self, event):
         message = event['message']
         sender = event['sender']
@@ -57,13 +58,10 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         formatted_timestamp = new_message.formatted_timestamp()
 
-        # 추가
         participants = chat_room.participants.exclude(id=sender.id)
         for participant in participants:
             notification = Notification(user=participant, chat_room=chat_room, message=new_message)
             notification.save()
-
-
 
         return formatted_timestamp
 
@@ -72,3 +70,51 @@ class ChatConsumer(AsyncWebsocketConsumer):
         users = get_user_model().objects.filter(id__in=user_ids)
         chat_room = ChatRoom.get_or_create_chat_room(users)
         return chat_room
+
+
+class NotificationConsumer(AsyncWebsocketConsumer):
+
+    async def connect(self):
+        self.room_group_name = '%s_notifications' % self.scope['user'].username
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        print("Received data:", text_data_json)
+        message = text_data_json['message']
+        formatted_timestamp = await self.save_message(message)
+        room_name = text_data_json['room_name']
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_notification',
+                'message': message,
+                'sender': self.scope['user'].username,
+                'formatted_timestamp': formatted_timestamp,
+                'room_name': room_name,
+            }
+        )
+
+    async def chat_notification(self, event):
+        message = event['message']
+        sender = event['sender']
+        formatted_timestamp = event['formatted_timestamp']
+        room_name = event['room_name']
+
+        await self.send(text_data=json.dumps({
+            'message': message,
+            'sender': sender,
+            'formatted_timestamp': formatted_timestamp,
+            'room_name': room_name,
+        }))
--- planethelper/asgi.py
+++ planethelper/asgi.py
@@ -18,7 +18,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'planethelper.settings')
 
 application = get_asgi_application()
 
-
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
