d0bd3db897565126b458cf4f0b4b8dae248cdab8
iblug <1115jkb@gmail.com>
2023-06-08 11:44:35 +0900

    Add user_cart quantity

---
+++ carts/static/carts/js/cart_quantity.js
@@ -0,0 +1,46 @@
+const quantityBtn = document.querySelectorAll('button[name="quantity_btn"]')
+const csrfToken = document.querySelector('[name=csrfmiddlewaretoken]').value
+
+quantityBtn.forEach(function(e) {
+  e.addEventListener('click', function(event) {
+    event.preventDefault()
+    const productId = e.dataset.productId
+    const quantityValue = e.dataset.quantityValue
+    if (isAuthenticated) {
+      user_modify_quantity(productId, quantityValue)
+    } else {
+      guest_modify_quantity()
+    }
+  })
+})
+
+
+function user_modify_quantity(productId, quantityValue) {
+  console.log(Number(quantityValue))
+  axios({
+    method: 'POST',
+    url: '/carts/modify_quantity/',
+    headers: {'X-CSRFToken': csrfToken},
+    data: JSON.stringify({
+      productId,
+      quantityValue,
+    })
+  })
+    .then((response) => {
+      const productQuantityDiv = document.getElementById(`product_count-${productId}`)
+      const subTotalSpan = document.getElementById(`sub_total-${productId}`)
+      const totalSpan = document.getElementById('total')
+      // console.log(response)
+      // console.log(response.data.subTotal)
+      // console.log(response.data.total)
+      // subTotalSpan.innerHTML = response.data.subTotal
+      productQuantityDiv.textContent = response.data.quantity
+      subTotalSpan.innerHTML = response.data.subTotal.toLocaleString()
+      totalSpan.innerHTML = response.data.total.toLocaleString()
+    })
+}
+
+
+function guest_modify_quantity() {
+
+}
--- carts/views.py
+++ carts/views.py
@@ -49,6 +49,33 @@ def add_item(request):
     return JsonResponse({'success': 'Item added to cart'})
 
 
+def modify_quantity(request):
+    jsonObject = json.loads(request.body)
+    product_pk = jsonObject['productId']
+    quantityValue = int(jsonObject['quantityValue'])
+
+    product = Product.objects.get(pk=product_pk)
+    user_cart, created = Cart.objects.get_or_create(user=request.user)
+    cart_item, created = CartItem.objects.get_or_create(cart=user_cart, product=product)
+    print(product)
+    if quantityValue == 1:
+        cart_item.quantity += 1
+    elif quantityValue == -1 and cart_item.quantity > 1:
+        cart_item.quantity -= 1
+
+    cart_item.save()
+    context = {
+        # 'product_pk': product_pk,
+        # 'price': product.price,
+        'quantity': cart_item.quantity,
+        'subTotal': cart_item.sub_total(),
+        'total': user_cart.total(),
+        # 'user_cart': user_cart,
+    }
+    return JsonResponse(context)
+
+
+# localstorage
 def product_info(request, product_id):
     product = Product.objects.get(pk=product_id)
     image = product.images.first()
@@ -61,6 +88,7 @@ def product_info(request, product_id):
         'image': image.image.url,
     }
     return JsonResponse(data)
+
 
 @login_required
 def order_page(request):
