a2b744d7709fe644f95a9a211c51c211fa2cca28
iblug <1115jkb@gmail.com>
2023-06-13 03:24:20 +0900

    Fix badge bug

--- carts/static/carts/js/base_cart.js
+++ carts/static/carts/js/base_cart.js
@@ -1,20 +1,22 @@
 const userAuth = document.getElementById("user-auth")
 const isAuthenticated = userAuth.dataset.authenticated === "True"
 
-window.onload = function() {
-  // localStorage에 cart key 값 확인
-  let cart = localStorage.getItem("cart")
+if (!isAuthenticated) {
+  window.onload = function() {
+    // localStorage에 cart key 값 확인
+    let cart = localStorage.getItem("cart")
 
-  if (cart === null || cart === "null") {
-      // localStorage에 cart key 값이 없는 경우 (빈 배열 생성)
-      cart = []
-      localStorage.setItem("cart", JSON.stringify(cart))
-  }
-  else {
-      // localStorage에 cart key 값이 있는 경우, JSON 문자열을 객체로 변환
-      cart = JSON.parse(cart)
-  }
-  const countBadge = document.getElementById('count_badge')
+    if (cart === null || cart === "null") {
+        // localStorage에 cart key 값이 없는 경우 (빈 배열 생성)
+        cart = []
+        localStorage.setItem("cart", JSON.stringify(cart))
+    }
+    else {
+        // localStorage에 cart key 값이 있는 경우, JSON 문자열을 객체로 변환
+        cart = JSON.parse(cart)
+    }
+    const countBadge = document.getElementById('count_badge')
 
-  countBadge.textContent = cart.length
+    countBadge.textContent = cart.length
+  }
 }
