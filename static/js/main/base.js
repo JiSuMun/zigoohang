80cb215406ad5918a8845eb6aca71a68102d8f56
iblug <1115jkb@gmail.com>
2023-06-19 00:23:55 +0900

    Add main theme btn

--- static/js/main/base.js
+++ static/js/main/base.js
@@ -18,6 +18,16 @@ function scrollToTop() {
   document.documentElement.scrollTop = 0;
 }
 
+// 테마 버튼
+document.getElementById('set_btn').addEventListener('click', function () {
+  var menuContainer = document.getElementById('theme_btn_box');
+  menuContainer.classList.toggle('theme_hidden');
+
+  // this.textContent = menuContainer.classList.contains('theme_hidden') ? '메뉴 열기' : '메뉴 닫기';
+});
+
+
+
 // 테마
 function getStoredTheme() {
   return localStorage.getItem("theme");
@@ -54,17 +64,17 @@ const darkThemeBtn = document.getElementById("darkThemeBtn");
 lightThemeBtn.addEventListener("click", () => {
   stylesheet.href = '/static/css/main/theme-light.css'
   setStoredTheme("light");
-  lightThemeBtn.classList.add('theme_icon_active')
-  darkThemeBtn.classList.remove('theme_icon_active')
-  userPrefThemeBtn.classList.remove('theme_icon_active')
+  lightThemeBtn.querySelector('img').classList.add('theme_icon_active')
+  darkThemeBtn.querySelector('img').classList.remove('theme_icon_active')
+  userPrefThemeBtn.querySelector('img').classList.remove('theme_icon_active')
 });
 
 darkThemeBtn.addEventListener("click", () => {
   stylesheet.href = '/static/css/main/theme-dark.css'
   setStoredTheme("dark");
-  lightThemeBtn.classList.remove('theme_icon_active')
-  darkThemeBtn.classList.add('theme_icon_active')
-  userPrefThemeBtn.classList.remove('theme_icon_active')
+  lightThemeBtn.querySelector('img').classList.remove('theme_icon_active')
+  darkThemeBtn.querySelector('img').classList.add('theme_icon_active')
+  userPrefThemeBtn.querySelector('img').classList.remove('theme_icon_active')
 
 });
 function applyOSTheme() {
@@ -76,21 +86,21 @@ const userPrefThemeBtn = document.getElementById("userPrefThemeBtn");
 
 userPrefThemeBtn.addEventListener("click", () => {
   applyOSTheme();
-  lightThemeBtn.classList.remove('theme_icon_active')
-  darkThemeBtn.classList.remove('theme_icon_active')
-  userPrefThemeBtn.classList.add('theme_icon_active')
+  lightThemeBtn.querySelector('img').classList.remove('theme_icon_active')
+  darkThemeBtn.querySelector('img').classList.remove('theme_icon_active')
+  userPrefThemeBtn.querySelector('img').classList.add('theme_icon_active')
 });
 
 // 초기 테마 설정 및 적용 (수정)
 if (getStoredTheme() === null) {
   applyOSTheme();
-  userPrefThemeBtn.classList.add('theme_icon_active')
+  userPrefThemeBtn.querySelector('img').classList.add('theme_icon_active')
 } else {
   setTheme(getPreferredTheme());
   if (getPreferredTheme() === 'light') {
-    lightThemeBtn.classList.add('theme_icon_active')
+    lightThemeBtn.querySelector('img').classList.add('theme_icon_active')
   } else if (getPreferredTheme() === 'dark') {
-    darkThemeBtn.classList.add('theme_icon_active')
+    darkThemeBtn.querySelector('img').classList.add('theme_icon_active')
   }
 }
 // 초기 테마 설정 및 적용
